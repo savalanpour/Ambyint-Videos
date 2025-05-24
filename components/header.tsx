388d0e74ca1f09b7191eb4500fff36752b4416ad
savalanpour <m.savalanpour@gmail.com>
2025-05-24 14:12:52 -0600

    create header and menu component

---
+++ components/header.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { MenuItem } from "@/components/menu-item";
+import Logo from "@/public/images/logo-white.png";
+import Typo from "@/public/images/logo-text.png";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-black shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-lg font-bold text-orange-600">
+          <Link href="/public" className="flex items-center">
+            <Image height={90} src={Logo} alt="Ambyint Videos Logo" />
+            <Image height={50} src={Typo} alt="Ambyint Videos Text" />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <nav className="flex space-x-6">
+            <MenuItem link="/" name="Home" />
+            <MenuItem link="/about-us" name="About Us" />
+            <MenuItem link="/core-features" name="Core Features" />
+            <MenuItem link="/contact-us" name="Contact Us" />
+          </nav>
+          
+          <div className="flex items-center gap-4 text-white">Login Button</div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ components/menu-item.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+type MenuItemPropsType = {
+  link: string;
+  name: string;
+};
+
+export const MenuItem = (props: MenuItemPropsType) => {
+  const { link, name } = props;
+  const pathname = usePathname();
+  
+  const isActiveRoute = (route: string) => {
+    return pathname === route;
+  };
+  
+  return (
+    <Link
+      href={link}
+      className={
+        isActiveRoute(link)
+          ? "font-bold text-orange-600"
+          : "text-gray-400 hover:text-orange-600"
+      }
+    >
+      {name}
+    </Link>
+  );
+};
